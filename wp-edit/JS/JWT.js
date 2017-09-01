3c75faa2962724084dd75318a3ec0522aebf3b87
Daisuke-sama <otaku.daisuke@gmail.com>
2017-09-01 21:19:33 +0300

    Added complete logic of editing post title with login, and added logout.

--- wp-edit/JS/JWT.js
+++ wp-edit/JS/JWT.js
@@ -1,11 +1,19 @@
 (function ($) {
 
+    /**
+     * Variables section
+     */
+
     const RESTROOT = 'https://' + window.location.host + '/wp-json';
-    const $ENTRYTITLE = $('.post-title');
+    const $THETITLE = $('.post-title');
     const $LOGIN = $('#loginform');
     const $LOGOUT = $('#logout');
 
-    $LOGIN.toggle();
+    var theToken = sessionStorage.getItem('theToken');
+
+    /**
+     * Tuning section
+     */
 
     $('#login_button').click(function (e) {
         e.preventDefault();
@@ -16,6 +24,17 @@
 
     $('#logout').click(logout);
 
+    if (theToken === null) {
+        $LOGIN.toggle();
+    } else {
+        $LOGOUT.toggle();
+        editPostTitle();
+    }
+
+
+    /**
+     * Functions section
+     */
 
     function login(username, password) {
         $.ajax({
@@ -30,6 +49,8 @@
                 sessionStorage.setItem('theToken', response.token);
                 $LOGIN.toggle();
                 $LOGOUT.toggle();
+
+                editPostTitle();
             })
             .fail(function (response) {
                 console.error("REST error.");
@@ -40,6 +61,51 @@
         sessionStorage.clear();
         $LOGIN.toggle();
         $LOGOUT.toggle();
+        $('.edit-button').remove();
+    }
+
+    function editPostTitle() {
+        $THETITLE.after('<button class="edit-button edit">Edit Title</button>'
+            + '<button class="edit-button save" style="display: none;">Save title</button>');
+
+
+        $('.edit-button.edit').click(function () {
+            let $currentTitle = $THETITLE.text();
+            $THETITLE.toggle();
+            $THETITLE.after('<input id="edit-title-input">');
+            $('#edit-title-input').val($currentTitle);
+            $(this).toggle();
+            $('.edit-button.save').toggle();
+        });
+
+        $('.edit-button.save').click(function () {
+            let $postID = $('.post').attr('data-id');
+            let $newTitle = $('#edit-title-input').val();
+
+            saveToDb($postID, $newTitle);
+        });
+    }
+
+    function saveToDb(postID, newTitle) {
+        $.ajax({
+            url: RESTROOT + '/wp/v2/posts/' + postID,
+            method: 'POST',
+            beforeSend: function (xhr) {
+                xhr.setRequestHeader('Authorization', 'Bearer ' + sessionStorage.getItem('theToken'));
+            },
+            data: {
+                title: newTitle
+            }
+        }).done(function (response) {
+            console.log(response);
+            $('#edit-title-input').toggle();
+            $THETITLE.text(newTitle).toggle();
+            $('.navigation-list a[data-id="' + postID + '"]').text(newTitle);
+            $('.edit-button.edit').toggle();
+            $('.edit-button.save').toggle();
+        }).fail(function (response) {
+            console.log(response);
+        })
     }
 
 })(jQuery);
