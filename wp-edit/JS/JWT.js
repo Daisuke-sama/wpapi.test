0d708dd7919714cd209411df5a49ee6c6952592f
Daisuke-sama <otaku.daisuke@gmail.com>
2017-09-01 20:57:21 +0300

    Added login and logout via JWT.

--- wp-edit/JS/JWT.js
+++ wp-edit/JS/JWT.js
@@ -5,4 +5,41 @@
     const $LOGIN = $('#loginform');
     const $LOGOUT = $('#logout');
 
+    $LOGIN.toggle();
+
+    $('#login_button').click(function (e) {
+        e.preventDefault();
+        let $uname = $('#user_login').val();
+        let $upwd = $('#user_pass').val();
+        login($uname, $upwd);
+    });
+
+    $('#logout').click(logout);
+
+
+    function login(username, password) {
+        $.ajax({
+            url: RESTROOT + '/jwt-auth/v1/token',
+            method: 'POST',
+            data: {
+                'username': username,
+                'password': password
+            }
+        })
+            .done(function (response) {
+                sessionStorage.setItem('theToken', response.token);
+                $LOGIN.toggle();
+                $LOGOUT.toggle();
+            })
+            .fail(function (response) {
+                console.error("REST error.");
+            })
+    }
+
+    function logout() {
+        sessionStorage.clear();
+        $LOGIN.toggle();
+        $LOGOUT.toggle();
+    }
+
 })(jQuery);
