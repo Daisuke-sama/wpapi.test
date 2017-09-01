fea07cb1265aefe18a423b4d280909e3b6053e6b
Daisuke-sama <otaku.daisuke@gmail.com>
2017-09-01 20:56:55 +0300

    Added a stubborn for application that used WP REST to edit post titles into /wp-edit folder.

---
+++ wp-edit/JS/JWT.js
@@ -0,0 +1,8 @@
+(function ($) {
+
+    const RESTROOT = 'https://' + window.location.host + '/wp-json';
+    const $ENTRYTITLE = $('.post-title');
+    const $LOGIN = $('#loginform');
+    const $LOGOUT = $('#logout');
+
+})(jQuery);
